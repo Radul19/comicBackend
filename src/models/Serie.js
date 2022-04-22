f6dcc9cec3a1fa59f91cdb0ae7927c8a090da2af
Radul19 <radulito19@gmail.com>
2022-04-22 12:59:40 -0400

    commit

--- src/models/Serie.js
+++ src/models/Serie.js
@@ -8,17 +8,6 @@ const SerieSchema = new Schema({
     ownerId: { type: String, required: true },
     picture_public_id: { type: String, required: true },
     caps: { type: Array, required: true },
-
-    // picture:{type:String,default:'/img/user.png'},
-    // username: { type: String, required: true },
-    // description: { type: String, default:'' },
-    // email: { type: String, required: true },
-    // password: { type: String, required: true },
-    // followers: { type: Array, default: '0' },
-    // following: { type: Array, default: '0' },
-    // hidden: { type: Boolean },
-    // notifyTotal:{type:Number,default:0},
-    // notifySeen:{type:Number,default:0}
 })
 
 
--- src/models/User.js
+++ src/models/User.js
@@ -7,20 +7,12 @@ const UserSchema = new Schema({
     email: { type: String, required: true },
     admin: { type: Boolean, required: true },
     id: { type: String, required: true },
-    picture: { type: String, default: 'https://res.cloudinary.com/comicseries/image/upload/v1649827898/imgThumb_svogrq.png', required: true },
-    // username: { type: String, required: true },
-    // description: { type: String, default:'' },
-    // email: { type: String, required: true },
-    // password: { type: String, required: true },
-    // followers: { type: Array, default: '0' },
-    // following: { type: Array, default: '0' },
-    // hidden: { type: Boolean },
-    // notifyTotal:{type:Number,default:0},
-    // notifySeen:{type:Number,default:0}
+    picture: { type: String, default: "https://res.cloudinary.com/comicseries/image/upload/v1649827898/imgThumb_svogrq.png" }
+
 })
 
 UserSchema.methods.encryptPassword = async (password) => {
-    /// Se genera primero la "salt" que es como el algoritmo con el que se va a encriptar
+
     const salt = bcrypt.genSaltSync()
     return await bcrypt.hash(password, salt);
 };
